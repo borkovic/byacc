7053c6eb56dd545c51c1422103fb5196bf88c789
Drazen Borkovic <borkovic@nomail>
2019-07-11 23:08:47 -0700

    New tests

---
+++ test/yacc/test-varsyntax_calc1.tab.h
@@ -0,0 +1,23 @@
+#ifndef YYTOKEN_IS_DECLARED
+#define YYTOKEN_IS_DECLARED 1
+typedef enum varsyntax_calc1_token {
+    DREG = 257,
+    VREG = 258,
+    CONST = 259,
+    UMINUS = 260
+} varsyntax_calc1_token;
+#endif /* !YYTOKEN_IS_DECLARED */
+#ifdef YYSTYPE
+#undef  YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
+#endif
+#ifndef YYSTYPE_IS_DECLARED
+#define YYSTYPE_IS_DECLARED 1
+typedef union YYSTYPE
+{
+	int ival;	/* dreg & vreg array index values*/
+	double dval;	/* floating point values*/
+	INTERVAL vval;	/* interval values*/
+} YYSTYPE;
+#endif /* !YYSTYPE_IS_DECLARED */
+extern YYSTYPE varsyntax_calc1_lval;
